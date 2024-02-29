108a77bec4bd7a99c252cd342faa0f23d77cd85e
Perkles <davifernandosousadias18@gmail.com>
2024-02-29 14:20:30 -0300

    lógica de soma de matrizes test

--- test-aplic/test.py
+++ test-aplic/test.py
@@ -1,12 +1,23 @@
-matriz = []
-for i in range(5):
-    lista = []
-    for j in range(3):
-        add_number = 10
-        lista.append(add_number)
-    matriz.append(lista)
-print(matriz)
-print(" ")
-for k in matriz:
-    for j in matriz:
-        print()
+matriz1= [
+    [1,2,3,4]
+
+]
+
+matriz2= [
+    [1,4,3,4]
+]
+
+
+
+# deixar matriz só com um array
+ma = []
+
+for valores_matriz_one in matriz1: 
+    for valores_matriz_two in matriz2:
+        matriz_new =[]
+        for valores in range(4):
+            new = valores_matriz_one[valores] + valores_matriz_two[valores]
+            matriz_new.append(new)
+        print(matriz_new)
+
+
