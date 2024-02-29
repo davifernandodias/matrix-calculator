aaf4893787b67200a6b5afad08f68da7b1d239d1
Perkles <davifernandosousadias18@gmail.com>
2024-02-29 16:13:07 -0300

    calculadora v1 finalizada

--- test-aplic/limpa.py
+++ test-aplic/limpa.py
@@ -16,9 +16,8 @@ class calculadora_matrizes: # dimensoes
                 add=int(input("adiciona numero: "))
                 lista.append(add)
             self.matriz_one.append(lista)
-        print("deu certo")
-         
-        
+        print('''
+                criado com sucesso''')
 
         for i in range(self.n_linhas_dois): # segunda matriz
             lista_dois =[]
@@ -26,15 +25,34 @@ class calculadora_matrizes: # dimensoes
                 add_dois=int(input("adiciona numero: "))
                 lista_dois.append(add_dois)
             self.matriz_two.append(lista_dois)
-        print("deu certo")
-        
+        print('''
+                criado com sucesso''')
+          
+
+          
     def somar_matriz(self):
         result=[]
         for i in range(len(self.matriz_one)):
             result.append([])
             for j in range(len(self.matriz_one[0])):
                 result[i].append(self.matriz_one[i][j] + self.matriz_two[i][j])
-        print(result)
+        print(f"MATRIZ GERADA: {result}")
+
+    def subtrair_matriz(self):
+        result=[]
+        for i in range(len(self.matriz_one)):
+            result.append([])
+            for j in range(len(self.matriz_one[0])):
+                result[i].append(self.matriz_one[i][j] - self.matriz_two[i][j])
+        print(f"MATRIZ GERADA: {result}")
+
+    def multiplicar_matriz(self):
+        result=[]
+        for i in range(len(self.matriz_one)):
+            result.append([])
+            for j in range(len(self.matriz_one[0])):
+                result[i].append(self.matriz_one[i][j] * self.matriz_two[i][j])
+        print(f"MATRIZ GERADA: {result}")
                 
 
 menu = '''                MATRIZES CAL
@@ -55,25 +73,51 @@ while True:
         matriz1 = calculadora_matrizes(2,2,2,2)
         matriz1.criar_matriz()
         matriz1.somar_matriz()
+        char = input('''
+        
+        deseja fazer mais conta?: S/N
+        
+''')
+        if char.lower() == "s":
+            pass
+        elif char.lower() == "n":
+            break
+
+    elif opcao == 2:
+        print(f"vc escolheu opção subtrair as matrizes")
+        # ad1,d2,d3,d4 = input("digite as dimensoes: ").split(" ")
+        matriz1 = calculadora_matrizes(2,2,2,2)
+        matriz1.criar_matriz()
+        matriz1.subtrair_matriz()
+        char = input('''
+        
+        deseja fazer mais conta?: S/N
+        
+''')
+        if char.lower() == "s":
+            pass
+        elif char.lower() == "n":
+            break  
+
+    elif opcao == 3:
+        print(f"vc escolheu opção multiplicar as matrizes")
+        # ad1,d2,d3,d4 = input("digite as dimensoes: ").split(" ")
+        matriz1 = calculadora_matrizes(2,2,2,2)
+        matriz1.criar_matriz()
+        matriz1.multiplicar_matriz()
+        char = input('''
+        
+        deseja fazer mais conta?: S/N
+        
+''')
+        if char.lower() == "s":
+            pass
+        elif char.lower() == "n":
+            break
+
+    elif opcao == 3:
+        print("VOCE SAIU DA CALCULADORA...BYEE")
         break
 
-
-
-
-        
-# TODO INFORMAR MATRIZES DIMENSSOES DA MATRIZES 
-
-    # FUNCÃO PARA CRIAR MATRIZES 
-            # DIMENSÃO LINHA X COLUNA
-             # INPUT PECORRER VALOR E ADICIONAR A MATRIZ
-
-
-    # ESTRUTURA IF
-
-    # CONDIÇÃO +
-
-    # CONDIÇÃO -
-
-    # CONDIÇÃO *
 
 
